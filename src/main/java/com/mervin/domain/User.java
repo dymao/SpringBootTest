fb94f76c2dd7bc783441de08f5a442beef815601
Mervin <dymaosuper@163.com>
2017-07-23 16:54:57 +0800

    新增表单验证

--- src/main/java/com/mervin/domain/User.java
+++ src/main/java/com/mervin/domain/User.java
@@ -1,8 +1,13 @@
 package com.mervin.domain;
 
+import org.hibernate.validator.constraints.NotEmpty;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 /**
  * @author Mervin
@@ -16,10 +21,16 @@ public class User {
     @GeneratedValue
     private Integer id;
 
+    @Size(min = 2,max = 15,message = "用户名长度在2到15之间！")
+    @NotEmpty(message="用户名不能为空")
+    @Pattern(regexp = "[a-zA-Z]{1}[a-zA-Z0-9_]{1,14}",message = "用户名必须由字母数字下划线组成且开头必须是字母，不能超过16位！")
     private String name;
 
+    @NotEmpty(message="密码不能为空")
+    @Pattern(regexp = "[a-zA-Z0-9]{1,10}",message = "密码只能包含字母和数字！且长度不超过10！")
     private String password;
 
+    @Min(value = 18,message = "未满18岁！！！")
     private Integer age;
 
 
